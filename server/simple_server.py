1f57e06f3a0b712b55788531f0529450090bafe9
Stephen "Zero" Chappell <Blastus@users.noreply.github.com>
2017-12-14 10:05:58 -0600

    Cleans up the code for the complex and simple servers.

--- server/simple_server.py
+++ server/simple_server.py
@@ -1,17 +1,19 @@
 #! /usr/bin/env python3
-import socket, select
+import select
+import socket
+
 
 def main():
-    a = [socket.socket(socket.AF_INET, socket.SOCK_STREAM)]     # socket array
+    a = [socket.socket(socket.AF_INET, socket.SOCK_STREAM)]  # socket array
     a[0].bind(('', 8989))
     a[0].listen(5)
     while True:
-        for b in select.select(a, [], [])[0]:                   # ready socket
+        for b in select.select(a, [], [])[0]:  # ready socket
             if b is a[0]:
                 a.append(b.accept()[0])
             else:
                 try:
-                    c = b.recv(1 << 12)                         # sent message
+                    c = b.recv(1 << 12)  # sent message
                 except socket.error:
                     b.shutdown(socket.SHUT_RDWR)
                     b.close()
@@ -19,6 +21,7 @@ def main():
                 else:
                     for d in (d for d in a[1:] if d is not b):  # message sink
                         d.sendall(c)
+
 
 if __name__ == '__main__':
     main()
